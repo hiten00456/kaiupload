05643d13b0884233f573b10dc5fad6195a5fd290
DragonPower84 <patrobighnesh8@gmail.com>
2022-01-07 21:13:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ import os
 # dotenv.load_dotenv()
 
 class Config(object):
-  AUTH_USERS = os.environ.get("AUTH_USERS")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "2030766680:AAEBO6Slyd3RCeaCQKwlUXXCtmZkdi3BbTc")
-  API_ID = int(os.environ.get("API_ID", 3509841))
-  API_HASH = os.environ.get("API_HASH", "68a5f01af74e8ef888458fdaea3376eb")
+  AUTH_USERS = os.environ.get("OWNER", "1477711713")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN")
+  API_ID = int(os.environ.get("API_ID"))
+  API_HASH = os.environ.get("API_HASH")
   THUMBNAIL = os.environ.get("THUMBNAIL_URL", "https://telegra.ph/file/5bb45f4b5255252a67c07.jpg")
   CHANNEL_ID = int(os.environ.get("UPLOAD_CH_ID", -1001447866756))
   DL_LOCATION = "./downloads"
