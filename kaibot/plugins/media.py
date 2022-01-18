10f1b90d16b1520d66c165fd3d5eb612f8ec3524
DragonPower84 <patrobighnesh8@gmail.com>
2022-01-18 22:29:58 +0530

    Update media.py

--- kaibot/plugins/media.py
+++ kaibot/plugins/media.py
@@ -10,7 +10,7 @@ import shutil
 from pySmartDL import SmartDL
 from pyrogram.errors import FloodWait
 
-async def zipprocessfile(bot, message):
+def zipprocessfile(bot, message):
      url = message.text
      if url.find("zip"):
         m = await message.reply_text("Downloading Your File")
