cef6fd26135cba1a6fa9b3678a5b7d2d0f9936ac
DragonPower84 <patrobighnesh8@gmail.com>
2022-02-01 13:02:56 +0530

    Update uncompress.py

--- kaibot/helpers/uncompress.py
+++ kaibot/helpers/uncompress.py
@@ -1,14 +1,17 @@
+from ..helpers.progress import progress_for_pyrogram
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from .. import Anibot, LOGGER
 from config import Config
 import os, time
 import shutil
-
 import zipfile, rarfile, tarfile
 
 
+
+# Zip Process
 async def zipp(bot, com, msg, dl_path):
     filename = dl_path.split("/")[-1]
-    sm = await msg.reply(f"`{filename}` Downloaded Successfully in {tt}.")
+    sm = await msg.reply(f"`{filename}` Downloaded Successfully in {tt}.", quote=True)
     with zipfile.ZipFile(dl_path, 'r') as zipObj:
         zipObj.extractall("downloads")
         dir_name = dl_path.replace(".zip", "")
@@ -30,6 +33,7 @@ async def zipp(bot, com, msg, dl_path):
            shutil.rmtree(os.path.join(dir_name, list))
        else:
            c += 1
+    await st.delete()
     countt = await msg.reply_text(f"Found {c} files to upload")
     files = []
     for file in ok:
@@ -63,5 +67,60 @@ async def zipp(bot, com, msg, dl_path):
     time.sleep(6)
     await complete.edit("Completed `{filename}` Task For You")
 
-
-
+# Tar Process
+async def tarr(bot, com, msg, dl_path):
+    filename = dl_path.split("/")[-1]
+    sm = await msg.reply(f"`{filename}` Downloaded Successfully in {tt}.", quote=True)
+    with tarfile.TarFile(dl_path, 'r') as tarObj:
+        tarObj.extractall("downloads")
+        dir_name = dl_path.replace(".tar", "")
+    if not os.path.exists(dir_name):
+        await sm.edit("Got Some Error while extracting")
+        return
+    else:
+        st = await msg.reply("Processed The File Now Uploading Will Start Soon")
+        pass
+    ok = os.listdir(dir_name)
+    c = 0
+    for list in ok:
+       if list == "desktop.ini":
+           try:
+              os.remove(f"{dir_name}/desktop.ini")
+           except:
+              pass
+       elif os.path.isdir(os.path.join(dir_name, list)):
+           shutil.rmtree(os.path.join(dir_name, list))
+       else:
+           c += 1
+    countt = await msg.reply(f"Found {c} files to upload")
+    files = []
+    for file in ok:
+        file = os.path.join(dir_name, file)
+        files.append(file)
+    files.sort()
+    thum = "thumb.jpeg"
+    await countt.delete()
+    for file in files:
+          try:
+             start = time.time()
+             filenamelist = file.split("/")[-1]
+             channelmsg = await Anibot.send_message(chat_id=Config.CHANNEL_ID, text=f"Uploading - `{filenamelist}`")
+             check = await msg.reply(f"Uploading - `{filenamelist}`")
+            # LOGGER.info(f"Uploading - {file}")
+             await Anibot.send_document(
+                 chat_id=Config.CHANNEL_ID,
+                 document=file,
+                 caption=f"◎ `{filenamelist}`\n\n**⌬ Uploaded By @Anime_Troop**",
+                 thumb=thum,
+                 force_document=True,
+                 progress=progress_for_pyrogram,
+                 progress_args=(bot, check, "🅄🄿🄻🄾🄰🄳🄸🄽🄶", start)
+             )
+             time.sleep(2)
+             await channelmsg.delete()
+             await chech.delete()
+          except FloodWait as e:
+             time.sleep(e.x)
+    complete=await msg.reply("Completed The Task. Now Taking a Sleep Nap", quote=True)
+    time.sleep(6)
+    await complete.edit("Completed `{filename}` Task For You")
