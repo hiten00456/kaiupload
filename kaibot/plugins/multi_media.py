4468beb6a07cdfa17b653ad0f249b4ae4d876849
DragonPower84 <patrobighnesh8@gmail.com>
2022-02-02 00:25:48 +0530

    Update multi_media.py

--- kaibot/plugins/multi_media.py
+++ kaibot/plugins/multi_media.py
@@ -39,16 +39,16 @@ async def multi_up_file(bot, callback_query):
            return
        else:
            LOGGER.info(dl_path)
-           com = await m.edit(f"**Downloaded Successfully {filename} in {tt}")
-           time.sleep(2)
-           await com.delete()
+           com = await m.edit(f"Downloaded Successfully {filename} in {tt}")
+           time.sleep(3)
            if zipfile.is_zipfile(dl_path):
                 await zipp(bot, com, msg, dl_path)
            if rarfile.is_rarfile(dl_path):
                 await rarr(bot, com, msg, dl_path)
            if tarfile.is_tarfile(dl_path):
                 await tarr(bot, com, msg, dl_path)
-           com = await callback_query.message.reply_text("**Completed The Task. Now Taking a Sleep Nap of 7 secends**")
+           await com.delete()
+           com = await msg.reply_text("**Completed The Task. Now Taking a Sleep Nap of 7 secends**")
            time.sleep(7)
            if os.path.isdir("downloads"):
                 shutil.rmtree("downloads")
