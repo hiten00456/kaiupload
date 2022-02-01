00eb45228d1db0dd5e276639f2a248c9d38f1ad9
DragonPower84 <patrobighnesh8@gmail.com>
2022-02-02 00:52:38 +0530

    Update __init__.py

--- kaibot/__init__.py
+++ kaibot/__init__.py
@@ -9,7 +9,10 @@ logging.basicConfig(level=logging.INFO,
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 Anibot = Kai84AnimeBoT()
-AUTH_URSERS = []
-achats = Config.AUTH_USERS.split(" ")
-for chats in achats:
-    AUTH_USERS.append(int(chats))
+AUTH_USERS = set()
+if " " in Config.AUTH_USERS:
+    achats = Config.AUTH_USERS.split(" ")
+    for chats in achats:
+        AUTH_USERS.add(int(chats))
+else:
+    AUTH_USERS = int(Config.AUTH_USERS)
